16b044e40071e340cff50e9dce50e55125361fab
Kian Eliasi <kian.elbo@gmail.com>
2019-06-22 15:44:42 +0430

    datatypes and constants for the network's output added

--- sources/datatypes_package.vhd
+++ sources/datatypes_package.vhd
@@ -3,9 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 package datatypes_package is
+    type vector_2 is array(0 to 1) of real;
     type vector_4 is array(0 to 3) of real;
     type vector_8 is array(0 to 7) of real;
     type matrix_4_8 is array(0 to 3, 0 to 7) of real;
+    type matrix_8_2 is array(0 to 7, 0 to 1) of real;
     type matrix_8_8 is array(0 to 7, 0 to 7) of real;
 end datatypes_package;
 
